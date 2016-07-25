06633c4ddb9e42b8b09d4f47149c4f8e89cc47a5
fusion32 <marcopuzziello@gmail.com>
2016-07-24 21:16:26 -0300

    organized GL loader and fixed README files

--- code/renderer/gl/gl_loader.c
+++ code/renderer/gl/gl_loader.c
@@ -18,27 +18,28 @@ static GLboolean l_loaded = GL_FALSE;
 static GLuint l_extensionCount;
 static const char *l_pExtensions[GL_MAX_EXTENSION_COUNT];
 
+
 //
-#define GL_CORE_PROC(proc_type, proc_name) proc_type proc_name = NULL;
-#define GL_EXT_PROC(proc_type, proc_name) proc_type proc_name = NULL;
-#include "gl_loader.inl"
+// OpenGL function decl here
+//
 
 typedef void (*PFNGLVOIDFUNCTION)(void);
 
-#ifdef _WIN32
+#if defined(_WIN32)
 static HMODULE l_libgl = NULL;
-
 static GLboolean GL_OpenLib()
 {
 	l_libgl = LoadLibraryA("opengl32.dll");
 	return l_libgl != NULL;
 }
-
 static void GL_CloseLib()
 {
 	FreeLibrary(l_libgl);
 }
-
+static GLboolean GL_IsLibOpen()
+{
+	return (l_libgl != NULL);
+}
 static PFNGLVOIDFUNCTION GL_GetProcAddr(const char *procName)
 {
 	PFNGLVOIDFUNCTION proc;
@@ -51,29 +52,11 @@ static PFNGLVOIDFUNCTION GL_GetProcAddr(const char *procName)
 }
 #endif
 
-static GLboolean GL_LoadCore()
-{
-	GLboolean ret = GL_TRUE;
-
-	#define GL_CORE_PROC(proc_type, proc_name)								\
-		proc_name = (proc_type) GL_GetProcAddr(#proc_name);					\
-		if(proc_name == NULL){												\
-			LOG_ERROR("GL_LoadCore: Failed to load " #proc_name "!");		\
-			ret = GL_FALSE;													\
-		}
-	#include "gl_loader.inl"
-
-	if(ret == GL_FALSE){
-		#define GL_CORE_PROC(proc_type, proc_name) proc_name = NULL;
-		#include "gl_loader.inl"
-	}
-	return GL_FALSE;
-}
 
 GLboolean GL_LoadAPI()
 {
 	const char *version;
-	uint32_t i;
+	uint32_t i, res;
 
 	if(l_loaded)
 		return GL_TRUE;
@@ -100,55 +83,32 @@ GLboolean GL_LoadAPI()
 
 	LOG("OpenGL Version: %d.%d", gl_info.majorVersion, gl_info.minorVersion);
 
-	if(GL_LoadCore() == GL_FALSE){
-		LOG_ERROR("GL_LoadAPI: Unable to load OpenGL Core!");
-		return GL_FALSE;
-	}
 
 	glGetIntegerv(GL_NUM_EXTENSIONS, &l_extensionCount);
 	for(i = 0; i < l_extensionCount; ++i)
 		l_pExtensions[i] = glGetStringi(GL_EXTENSIONS, i);
 
+	// Load OpenGL Core
+	//=================================================================
+	#define GL_GET_PROC_ADDR(proc_type, proc_name)									\
+		proc_name = (proc_type) GL_GetProcAddr(#proc_name);							\
+		if(proc_name == NULL){														\
+			LOG_ERROR("GL_LoadAPI: Failed to load core function: %s!", #proc_name);	\
+			res = GL_FALSE;															\
+		}
+
+	res = GL_TRUE;
+	GL_GET_PROC_ADDR(PFNGLTEXIMAGE3DPROC, glTexImage3D);
+
+	#undef GL_GET_PROC_ADDR
+	//=================================================================
+
+	// Load OpenGL Extensions
+	//=================================================================
+	//=================================================================
+
+
 
 	l_loaded = GL_TRUE;
 	return GL_TRUE;
 }
-
-/*
-
-static const char *l_requiredExtensionsHIGH[] = {
-	// for debugging (should not be required but should be activable)
-	"GL_ARB_debug_output",
-
-	// essential
-	"GL_ARB_shader_draw_parameters",
-	"GL_ARB_draw_indirect",
-	"GL_ARB_multi_draw_indirect",
-
-	"GL_ARB_direct_state_access",
-	"GL_ARB_shader_storage_buffer_object",
-	"GL_ARB_buffer_storage",
-	"GL_ARB_texture_storage",
-	"GL_ARB_separate_shader_objects",
-
-	"GL_ARB_explicit_attrib_location",
-	"GL_ARB_explicit_uniform_location",
-	"GL_ARB_shading_language_420pack",
-};
-
-static const char *l_requiredExtensionsLOW[] = {
-	// for debugging (should not be required but should be activable)
-	"GL_ARB_debug_output",
-
-	// essential
-	"GL_ARB_sync",
-	"GL_ARB_framebuffer_object",
-	"GL_ARB_texture_storage",
-	"GL_EXT_texture_array",
-
-	"GL_ARB_explicit_attrib_location",
-	"GL_ARB_explicit_uniform_location",
-	"GL_ARB_shading_language_420pack",
-};
-
-*/
--- m1b_map/README.md
+++ m1b_map/README.md
@@ -1,3 +1,4 @@
-# m1b_map
+m1b_map
+=======
 
 It is still not set but the editor will probably save maps in xml and then this tool will be used to convert it to binary.
